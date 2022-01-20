9bd7c113ac9e5958bea5a5f471c81c57db01b6a8
qinyifang <fang6ang@outlook.com>
2022-01-20 17:04:58 +0800

    Train

--- model/train.py
+++ model/train.py
@@ -1,4 +1,6 @@
-from transformers import BertTokenizer, GPT2LMHeadModel, TextGenerationPipeline
+import torch
+from torch.nn.utils import clip_grad_norm_ as clip_grad
+from transformers import BertTokenizer, GPT2LMHeadModel, AdamW
 import pandas as pd
 from utils import TextData
 from torch.utils.data import DataLoader
@@ -9,19 +11,34 @@ ARG = argparse.ArgumentParser()
 
 ARG.add_argument('--epoch', type=int, default=140, help='Epoch num.')
 ARG.add_argument('--seed', type=int, default=98765, help='Random seed.')
+ARG.add_argument('--cuda', type=str, default=None, help='Cuda ID.')
 ARG.add_argument('--batch', type=int, default=1024, help='Training batch size.')
-ARG.add_argument('--data', type=str, default='', help='Training batch size.')
+ARG.add_argument('--lr', type=float, default=1e-5, help='Initial learning rate.')
+ARG.add_argument('--data', type=str, default='', help='Dataset directory.')
+ARG.add_argument('--pretrain_path', type=str, default='../cache_dir', help='Pretrain model path.')
+ARG.add_argument('--pretrain_name', type=str, default='uer/gpt2-chinese-cluecorpussmall', help='Pretrain model.')
 
 ARG = ARG.parse_args()
 
 
-def train(model_path, _loader):
-    tokenizer = BertTokenizer.from_pretrained('uer/gpt2-chinese-cluecorpussmall', cache_dir='../cache_dir')
-    model = GPT2LMHeadModel.from_pretrained('uer/gpt2-chinese-cluecorpussmall', cache_dir='../cache_dir')
+def train(_loader, arg):
+    device = torch.device('cpu' if arg.cuda is None else 'cuda:' + arg.cuda)
+
+    tokenizer = BertTokenizer.from_pretrained(arg.pretrain_name, cache_dir=arg.pretrain_path)
+    model = GPT2LMHeadModel.from_pretrained(arg.pretrain_name, cache_dir=arg.pretrain_path).to(device)
+    opt = AdamW(model.parameters(), lr=arg.lr)
     print('Model loaded, start training.')
+
     for seqs in _loader:
         input_dict = tokenizer(seqs, padding=True, truncation=True, return_tensors='pt')
-        outputs = model(**input_dict)
+        outputs = model(input_dict['input_ids'], labels=input_dict['input_ids'])
+        _, loss = outputs.logits, outputs.loss
+
+        opt.zero_grad()
+        loss.backward()
+        clip_grad(model.parameters(), 2.)
+        opt.step()
+        print(loss.item())
         break
 
 
@@ -30,5 +47,5 @@ if __name__ == '__main__':
     tst_data = pd.read_csv('../processed_data/' + ARG.data + 'test.csv').dropna()
     val_data = pd.read_csv('../processed_data/' + ARG.data + 'val.csv').dropna()
 
-    loader = DataLoader(TextData(trn_data), batch_size=4, shuffle=True)
-    train(0, loader)
+    loader = DataLoader(TextData(trn_data), batch_size=ARG.batch, shuffle=True)
+    train(loader, ARG)
