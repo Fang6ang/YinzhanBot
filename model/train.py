8892a37d7434a52dbf3dc84b04e63482f89030a6
qinyifang <fang6ang@outlook.com>
2022-01-25 15:30:56 +0800

    Add cron & model inference

--- model/train.py
+++ model/train.py
@@ -1,18 +1,18 @@
-import torch
+import torch, random, os, argparse, json
+from tqdm.auto import tqdm
 from torch.nn.utils import clip_grad_norm_ as clip_grad
 from transformers import BertTokenizer, GPT2LMHeadModel, AdamW
-import pandas as pd
-from utils import TextData
-from torch.utils.data import DataLoader
-import argparse
 
+import pandas as pd, numpy as np
+from utils import TextData, generate
+from torch.utils.data import DataLoader
 
 ARG = argparse.ArgumentParser()
 
 ARG.add_argument('--epoch', type=int, default=140, help='Epoch num.')
 ARG.add_argument('--seed', type=int, default=98765, help='Random seed.')
 ARG.add_argument('--cuda', type=str, default=None, help='Cuda ID.')
-ARG.add_argument('--batch', type=int, default=1024, help='Training batch size.')
+ARG.add_argument('--batch', type=int, default=2, help='Training batch size.')
 ARG.add_argument('--lr', type=float, default=1e-5, help='Initial learning rate.')
 ARG.add_argument('--data', type=str, default='', help='Dataset directory.')
 ARG.add_argument('--pretrain_path', type=str, default='../cache_dir', help='Pretrain model path.')
@@ -20,6 +20,13 @@ ARG.add_argument('--pretrain_name', type=str, default='uer/gpt2-chinese-cluecorp
 
 ARG = ARG.parse_args()
 
+
+def seed_torch(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
 
 def train(_loader, arg):
     device = torch.device('cpu' if arg.cuda is None else 'cuda:' + arg.cuda)
@@ -29,20 +36,31 @@ def train(_loader, arg):
     opt = AdamW(model.parameters(), lr=arg.lr)
     print('Model loaded, start training.')
 
-    for seqs in _loader:
-        input_dict = tokenizer(seqs, padding=True, truncation=True, return_tensors='pt')
-        outputs = model(input_dict['input_ids'], labels=input_dict['input_ids'])
-        _, loss = outputs.logits, outputs.loss
+    process_bar = tqdm(range(len(_loader) * 3 // arg.batch))
+    for _ in range(3):
+        for seqs in _loader:
+            input_dict = tokenizer(seqs, padding=True, truncation=True, return_tensors='pt')
+            outputs = model(input_dict['input_ids'].to(device), labels=input_dict['input_ids'].to(device))
+            _, loss = outputs.logits, outputs.loss
 
-        opt.zero_grad()
-        loss.backward()
-        clip_grad(model.parameters(), 2.)
-        opt.step()
-        print(loss.item())
-        break
+            opt.zero_grad()
+            loss.backward()
+            clip_grad(model.parameters(), 2.)
+            opt.step()
+            process_bar.update(1)
+        
+    model.save_pretrained('../saved_model')
+    tokenizer.save_pretrained('../saved_model')
+    predict(arg)
 
+def predict(arg):
+    res = generate(device=arg.cuda)
+    with open('res.json', 'w') as f:
+        json.dump(res, f)
 
 if __name__ == '__main__':
+    seed_torch(ARG.seed)
+
     trn_data = pd.read_csv('../processed_data/' + ARG.data + 'train.csv').dropna()
     tst_data = pd.read_csv('../processed_data/' + ARG.data + 'test.csv').dropna()
     val_data = pd.read_csv('../processed_data/' + ARG.data + 'val.csv').dropna()
--- model/utils.py
+++ model/utils.py
@@ -1,5 +1,17 @@
+import torch, pandas as pd
+from transformers import BertTokenizer, GPT2LMHeadModel, TextGenerationPipeline
 from torch.utils.data import Dataset
 
+
+
+def generate(context='#3000000', device=None, path='../saved_model'):
+    tokenizer = BertTokenizer.from_pretrained(path)
+    model = GPT2LMHeadModel.from_pretrained(path)
+    ppl = TextGenerationPipeline(model, tokenizer, device=device)
+    model.to(torch.device(f'cuda:{device}' if device is not None else 'cpu'))
+    
+    gen_text = ppl(context, max_length=200, do_sample=True)
+    return gen_text
 
 class TextData(Dataset):
     def __init__(self, data):
